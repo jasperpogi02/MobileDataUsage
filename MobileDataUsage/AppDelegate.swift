3c6db4597a94e8f9a634ab2d4c2f6945846954a3
Jasper Goce <jaspergoce@comfortdelgro.com>
2021-06-13 00:28:50 +0800

    Added new awesome feature

--- MobileDataUsage/AppDelegate.swift
+++ MobileDataUsage/AppDelegate.swift
@@ -14,6 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print(" ** ** ")
+        print("*  *  *")
+        print("*     *")
+        print(" *   * ")
+        print("  * *  ")
+        print("   *   ")
         return true
     }
 
