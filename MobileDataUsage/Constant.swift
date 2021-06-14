437764fee13faa982923cfb00df4fbdbd7496bc1
jasperpogi02 <jaspergoce@gmail.com>
2021-06-14 14:06:59 +0800

    Added a year validity.

--- MobileDataUsage/Constant.swift
+++ MobileDataUsage/Constant.swift
@@ -10,5 +10,5 @@ import Foundation
 struct Constant {
     static let dataURL = "https://data.gov.sg/api/action/datastore_search"
     static let resourceID = "a807b7ab-6cad-4aa6-87d0-e283a7353a0f"
-    static let validYears = [ "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
+    static let validYears = [ "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021"]
 }
